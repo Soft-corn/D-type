86e5d7522e1e7a19adad574b947f7e192ee2d69b
Prikshit singh <prikshitsingh79@gmail.com>
2024-02-16 12:54:19 +0530

    Entry point defination

---
+++ D-type/Core/Application.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+
+namespace Dtype {
+	Application::Application()
+	{
+		std::cout << "Constructor called!";
+	}
+	void Application::Startup()
+	{
+		std::cout << "Function startup called";
+	}
+}
---
+++ D-type/Core/Application.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+#include<conio.h>
+
+namespace Dtype {
+
+	class Application {
+	public:
+		Application();
+		void Startup();
+
+	};
+}
